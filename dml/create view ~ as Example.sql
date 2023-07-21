879d5e0eaf6d4ce564d5b4359d22906f64ed3773
SeongGwangJu <seonggwangdev@gmail.com>
2023-07-21 17:20:27 +0900

    Procedure에 대한 공부내용 추가.

--- dml/create view ~ as Example.sql
+++ dml/create view ~ as Example.sql
@@ -8,9 +8,4 @@ select
 from
 	user_withdraw_tb uwt
     left outer join table1 t1 on(t1.user_id = uwt.user_id)
-    left outer join table2 t2 on(t2.t1_id = t1.t1_id);
-
-select 
-	*
-from
-	user_withdraw_view;
+    left outer joi
